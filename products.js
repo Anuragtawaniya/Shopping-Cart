9dc7d0445a9adf94baba05d6108a7863ac8dee19
anuragtawaniya <53399643+Anuragtawaniya@users.noreply.github.com>
2023-12-29 15:53:43 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,28 @@
+const product = [
+    {
+        id:1,
+        name:"shirt",
+        size:"xxl",
+        price:1700,
+    },
+    {
+        id:2,
+        name:"shirt",
+        size:"xxl",
+        price:1300,
+    },
+    {
+        id:3,
+        name:"shirt",
+        size:"xxl",
+        price:1200,
+    },
+    {
+        id:4,
+        name:"shirt",
+        size:"xxl",
+        price:1500,
+    },
+
+]
+export default {product};
